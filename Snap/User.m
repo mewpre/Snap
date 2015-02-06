9e859fe5ebfbf50d77be1b31bb28c19d1a15a62a
Chris Giersch <chgiersch@Chriss-MacBook-Pro.local>
2015-02-05 19:53:23 -0800

    organized and added 'segue' stuff

--- Snap/User.m
+++ Snap/User.m
@@ -24,8 +24,6 @@
 
 
 
-
-
 + (void)retrieveRecent48HourPhotosFromUser:(User *)user withCompletion:(void(^)(NSArray *photosArray))Complete
 {
     PFRelation *relation = [user relationForKey:@"photos"];
